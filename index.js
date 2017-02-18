b3d87ab4aa2e83af1aa6ffa99b0d3960f03cf53d
Jay Patel <0807Jpatel@gmail.com>
2017-02-18 14:26:23 -0500

    added possible messages from user

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.sets('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({entended: true}));
 app.use(bodyParser.json());
 
+
+let token = "some token";
+
+
 app.get('/', function(req, res){
     res.send("A bot that helps find hackathon events");
 });
@@ -21,7 +25,24 @@ app.get('/webhook', function(req, res){
 });
 
 app.post('/webhook', function(req, res){
+    let message_events = req.body.entry[0].messaging;
+    for(let 1 = 0; i < message_events.length; i++){
+	let event = message_events[i];
+        let sender = event.sender.id;
+	if(event.message && event.message.text){
+		let text = event.message.text;
+		if(text == "Upcoming Hackathons"){
 
+		}else if(text == "Hackathons in "){
+
+		}else if{ text == "Hackathon this months"){
+
+		}else{
+
+		}
+	}
+	
+    }
 });
 
 
