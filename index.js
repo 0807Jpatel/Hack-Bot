794aad41c1dcc8c5b7e737212253c4a2cc6456db
Jay Patel <0807Jpatel@gmail.com>
2017-02-19 08:05:26 -0500

    testing image

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 var webshot = require('webshot');
-var phantomPath = require('phantomjs');
 AWS.config.loadFromPath('./config.json');
 
 var exec = require('child_process').exec;
