a06c88cef245438dfb866d8e4548354da494a385
Jay Patel <0807Jpatel@gmail.com>
2017-02-19 00:59:26 -0500

    added more function

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ docClient.scan(params, function (err, data){
                             "subtitle":description,
                             "default_action": {
                                 "type": "web_url",
-                                "url":  hackathon.link,
+                                "url":  "https://masseyhacks.ca/",
                                 "messenger_extensions": true,
                                 "webview_height_ratio": "tall",
                                 "fallback_url": hackathon.facebookURL
@@ -113,7 +113,7 @@ docClient.scan(params, function (err, data){
                             "buttons":[
                             { 
                                 "type":"web_url",
-                                "url": hackathon.link,
+                                "url": "https://masseyhacks.ca/",
                                 "title":"View Their Website"
                             }           
                         ]      
